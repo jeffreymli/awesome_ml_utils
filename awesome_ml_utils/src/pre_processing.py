1e2ca141136c8ff83df1df0fee2ebaa50345b2ee
jeffreymli <jeff.li@doordash.com>
2020-04-26 12:39:05 -0700

    test drop outliers

--- awesome_ml_utils/src/pre_processing.py
+++ awesome_ml_utils/src/pre_processing.py
@@ -1,32 +1,39 @@
 import pandas as pd
 from scipy import stats
 import numpy as np
+import logging
+
 
 def missing_values_table(df):
+    """
+    """
     mis_val = df.isnull().sum()
     mis_val_percent = 100 * df.isnull().sum() / len(df)
 
     mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
     mis_val_table_ren_columns = mis_val_table.rename(
-    columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        columns={0: 'Missing Values', 1: '% of Total Values'})
 
     mis_val_table_ren_columns = mis_val_table_ren_columns[
-        mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
-    '% of Total Values', ascending=False).round(1)
+        mis_val_table_ren_columns.iloc[:, 1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
 
     print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"
-           "There are " + str(mis_val_table_ren_columns.shape[0]) + 
+           "There are " + str(mis_val_table_ren_columns.shape[0]) +
            " columns that have missing values.")
 
     return mis_val_table_ren_columns
 
 
 class OutlierImputation:
+    """
+
+    """
     def __init__(self, df, cont_cols, std_threshold):
         self.df = df
         self.std_threshold = std_threshold
         self.outliers = None
-        
+
         if isinstance(cont_cols, str):
             self.cont_cols = [cont_cols]
         else:
@@ -36,88 +43,146 @@ class OutlierImputation:
         if isinstance(self.cont_cols, str):
             self.cont_cols = [self.cont_cols]
 
-        assert set(self.cont_cols).issubset(self.df.columns), 'Specified columns do not exist in dataset'
+        assert set(
+            self.cont_cols).issubset(
+            self.df.columns), 'Specified columns do not exist in dataset'
 
         self.cont_data = self.df[self.cont_cols].copy()
         self.z = pd.DataFrame(np.abs(stats.zscore(self.cont_data)))
         self.z.columns = self.cont_data.columns
         self.outliers = self.z[self.z[self.cont_cols] > self.std_threshold]
 
-
     def fit(self):
         self._get_outliers()
 
-    def _calculate_transform(self, impute_type, upper_bound, lower_bound, custom_function = None):
+    def _calculate_transform(
+            self,
+            impute_type,
+            upper_bound,
+            lower_bound,
+            custom_function=None):
 
         if upper_bound is None:
-            ## Three standard deviations
+            # Three standard deviations
             upper_bound = 99.85
 
         if lower_bound is None:
-            lower_pound = 0.15
-        
+            lower_bound = 0.15
+
         for col in self.cont_cols:
 
-            lower = np.percentile(self.df[col], lower_bound)
-            upper = np.percentile(self.df[col], upper_bound)
+            original_df = self.df.copy()
+
+            lower = np.percentile(original_df[col], lower_bound)
+            upper = np.percentile(original_df[col], upper_bound)
 
             col_name = str(col) + '_out_imp'
+            if impute_type == 'winsor':
+                print(
+                    "Column name {0} is now called {1} with imputation".format(
+                        str(col), col_name))
 
-            logging.info("Column name {0} is now called {1} with imputation".format(str(col),col_name))
-
-            if impute_type = 'winsor':
-                self.df.loc[:,col_name] = np.where(self.df[col] > upper, upper, self.df[col])
-                self.df.loc[:,col_name] = np.where(self.df[col] > lower, lower, self.df[col])
-
-            if impute_type = 'drop':
-
-                self.df.loc[:,col] = np.where(self.df[col] > upper, 'drop_999999', self.df[col])
-                self.df.loc[:,col] = np.where(self.df[col] > lower, 'drop_999999', self.df[col])
-                self.df = self.df[self.df[col] != 'drop_999999'].copy()
-
-            if impute_type = 'custom':
-
-                self.df.loc[:,col_name] = np.where(self.df[col] > upper, custom_function(self.df[col]), self.df[col])
-                self.df.loc[:,col_name] = np.where(self.df[col] > lower, custom_function(self.df[col]), self.df[col])
+                self.df.loc[:, col_name] = np.where(
+                    self.df[col] > upper, upper, self.df[col])
                 
+                self.df.loc[:, col_name] = np.where(
+                    self.df[col_name] < lower, lower,self.df[col_name])
 
-    def transform(self, impute_type, custom_function = None, upper_bound_perc = None, lower_bound_perc = None, **kwargs):
-        
-        if impute_type = 'winsor' or impute_type is None:
+            if impute_type == 'drop':
+
+                total_length = len(self.df[col])
+                self.df = self.df[self.df[col] <= upper].copy()
+                dropped_upper_counts = total_length - len(self.df[col].values)
+
+                print("Dropping {0} Upper Bound Values for column {1}".format(dropped_upper_counts, col))
+
+                self.df = self.df[self.df[col] >= lower].copy()
+                dropped_lower_counts = total_length - dropped_upper_counts - len(self.df[col])                
+                
+                print("Dropping {0} Lower Bound Values for column {1}".format(dropped_lower_counts, col))
+
+                print(
+                    "We have dropped a total of {0} values from column {1} ".format(
+                        str(dropped_lower_counts + dropped_upper_counts), col_name))
+                print('\n')
+            if impute_type == 'custom':
+                print(
+                    "Column name {0} is now called {1} with imputation".format(
+                        str(col), col_name))
+                self.df.loc[:, col_name] = np.where(
+                    self.df[col] > upper, custom_function(self.df[col]), self.df[col])
+                self.df.loc[:, col_name] = np.where(
+                    self.df[col] > lower, custom_function(self.df[col]), self.df[col])
+
+
+    def transform(
+            self,
+            impute_type,
+            custom_function=None,
+            upper_bound_perc=None,
+            lower_bound_perc=None,
+            **kwargs):
+
+        if impute_type == 'winsor' or impute_type is None:
             print("Applying Winsor transformation.......")
             if upper_bound_perc is None:
-                print("No upper bound percentile specified. Using default 3 standard deviations")
+                print(
+                    "No upper bound percentile specified. Using default 3 standard deviations")
             if lower_bound_perc is None:
-                print("No lower bound percentile specified. Using default 3 standard deviations")
-            self._calculate_transform(impute_type, upper_bound_perc, lower_bound_perc)
+                print(
+                    "No lower bound percentile specified. Using default 3 standard deviations")
+            self._calculate_transform(
+                impute_type, upper_bound_perc, lower_bound_perc)
 
-        if impute_type = 'drop':
-            print("Dropping all outliers. Be careful with this method......")
+        if impute_type == 'drop':
+            print("Dropping all outliers........")
+            print('\n')
+
+            if len(self.cont_cols) > 1:
+                print("You specified more than one column.")
+                print("Be careful with this method. We will drop all rows containing outliers starting with the first column.....")
+                print("This means we may be dropping more values than we want")
+                print('\n')
+
             if upper_bound_perc is None:
-                print("No upper bound percentile specified. Using default 3 standard deviations")
+                print(
+                    "No upper bound percentile specified. Using default 3 standard deviations")
+            
             if lower_bound_perc is None:
-                print("No lower bound percentile specified. Using default 3 standard deviations")
-            self._calculate_transform(impute_type, upper_bound_perc, lower_bound_perc)
+                print(
+                    "No lower bound percentile specified. Using default 3 standard deviations")
+            self._calculate_transform(
+                impute_type, upper_bound_perc, lower_bound_perc)
 
-        if impute_type = 'custom':
+        if impute_type == 'custom':
             print("Using a custom function")
             if upper_bound_perc is None:
-                print("No upper bound percentile specified. Using default 3 standard deviations")
+                print(
+                    "No upper bound percentile specified. Using default 3 standard deviations")
             if lower_bound_perc is None:
-                print("No lower bound percentile specified. Using default 3 standard deviations")
-            self._calculate_transform(impute_type, upper_bound_perc, lower_bound_perc, **kwargs)
+                print(
+                    "No lower bound percentile specified. Using default 3 standard deviations")
+            self._calculate_transform(
+                impute_type,
+                upper_bound_perc,
+                lower_bound_perc,
+                custom_function,
+                **kwargs)
 
+        return self.df.copy()
 
     def get_outlier_report(self):
 
         report = pd.DataFrame(self.outliers.count())
-        report.loc[:,'outliers_pct'] = self.outliers.count()/self.df.count()
+        report.loc[:, 'outliers_pct'] = self.outliers.count() / self.df.count()
         report['mean'] = self.cont_data[self.outliers.notnull()].mean()
         report['min'] = self.cont_data[self.outliers.notnull()].min()
         report['max'] = self.cont_data[self.outliers.notnull()].max()
-        report.columns = ['outlier_count','outliers_pct','outlier_mean','outlier_min','outlier_max']
+        report.columns = [
+            'outlier_count',
+            'outliers_pct',
+            'outlier_mean',
+            'outlier_min',
+            'outlier_max']
 
         return report
-        
-
-            
--- awesome_ml_utils/tests/test_pre_processing.py
+++ awesome_ml_utils/tests/test_pre_processing.py
@@ -4,8 +4,6 @@ import sys
 sys.path.insert(1, '../src/')
 from pre_processing import *
 
-
-
 def test_missing_values_table():
 	X = pd.DataFrame(np.random.randn(1000, 4), columns=list('ABCD'))
 
@@ -14,30 +12,68 @@ def test_missing_values_table():
 	X['C'].loc[400:] = np.nan ## 600
 	X['D'].loc[900:] = np.nan ## 100
 
-	missing_values_table = missing_values_table(X)
+	mv = missing_values_table(X)
+	
+	mv[mv.index == 'A']['Missing Values']
 
-	assert c['Missing Values'].iloc[0] == 990
-	assert c['Missing Values'].iloc[0] == 500
-	assert c['Missing Values'].iloc[0] == 600
-	assert c['Missing Values'].iloc[0] == 100
+	assert mv[mv.index == 'A']['Missing Values'][0] == 990
+	assert mv[mv.index == 'B']['Missing Values'][0] == 500
+	assert mv[mv.index == 'C']['Missing Values'][0] == 600
+	assert mv[mv.index == 'D']['Missing Values'][0] == 100
 
-def test_detect_outliers():
+def test_outlier_report():
 
 	X = pd.DataFrame(np.random.randn(10000, 4), columns=list('ABCD'))
 	outliers = [99999,99999,99999,99999]
 
 	o = OutlierImputation(X, cont_cols= 'C',std_threshold = 3)
+	o.fit()
 	report = o.get_outlier_report()
-	print(report)
 
+def test_outlier_transformations():
 
+	X = pd.DataFrame(np.random.randn(10000, 4), columns=list('ABCD'))
 
+	### Winsor
+	o = OutlierImputation(X, cont_cols= 'C',std_threshold = 2)
+	o.fit()
 
+	o.get_outlier_report()
+	data = o.transform(impute_type = 'winsor', upper_bound_perc = 97.5, lower_bound_perc = 2.5)
 
+	upper_bound = np.percentile(X['C'], 97.5)
+	lower_bound = np.percentile(X['C'], 2.5)
+
+	upper_bound_counts = X[X['C'] > upper_bound]['C']
+	lower_bound_counts = X[X['C'] < lower_bound]['C']
+
+	c = data[data['C'] > upper_bound]['C_out_imp']
+	
+	## Check winsor counts
+	assert len(data[data['C'] > upper_bound]['C_out_imp']) == len(upper_bound_counts)
+	assert len(data[data['C'] < lower_bound]['C_out_imp']) == len(lower_bound_counts)
+
+	## Check winsor values
+	assert round(np.mean(data[data['C'] < lower_bound]['C_out_imp']),3) == round(lower_bound,3)
+	assert round(np.mean(data[data['C'] > upper_bound]['C_out_imp']),3) == round(upper_bound,3)
+	
+	## Check winsor imputed value
+	assert max(data['C_out_imp']) == upper_bound
+	assert min(data['C_out_imp']) == lower_bound
+
+	### Check drop counts
+	data = o.transform(impute_type = 'drop', upper_bound_perc = 97.5, lower_bound_perc = 2.5)
+	assert len(upper_bound_counts) + len(lower_bound_counts) == len(X) - len(data)
+
+	### Check multiple drop columns
+	o = OutlierImputation(X, cont_cols= ['C','A','B'] ,std_threshold = 2)
+	o.fit()
+	data = o.transform(impute_type = 'drop', upper_bound_perc = 97.5, lower_bound_perc = 2.5)
 
 
 
 if __name__=='__main__':
-	test_detect_outliers()
+	# test_outlier_report()
+	test_outlier_transformations()
 	
 
